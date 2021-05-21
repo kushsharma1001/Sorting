eb1009d860d452334f292b86b1d74f89df77dd88
Kush Sharma <kushsharma1001@gmail.com>
2021-05-21 19:49:06 +0530

    Update Insertion.java

--- Insertion.java
+++ Insertion.java
@@ -6,7 +6,15 @@ class Insertion
 			{ System.out.print(arr[k]+" ");}							
 			System.out.println();
 			
-			for(int i=1;i<arr.length;i++)
+		 	for(int i=1;i<arr.length;i++)
+			{		for(int j=i;j>0 && arr[j]<arr[j-1];j--)
+					{		int temp=arr[j];
+						 	arr[j]=arr[j-1];
+						 	arr[j-1]=temp;
+					}
+			}
+			
+		 /*for(int i=1;i<arr.length;i++)
 			{		for(int j=i;j>0;j--)
 					{	if(arr[j]<arr[j-1])
 						{	int temp=arr[j];
@@ -14,17 +22,7 @@ class Insertion
 						 	arr[j-1]=temp;
 						}
 					}
-			}
-		/*		for (i = 1; i < length; i++)
-				{      j = i;
-					while (j > 0 && arr[j - 1] > arr[j]) 
-					{       	tmp = arr[j];
-						arr[j] = arr[j - 1];
-						arr[j - 1] = tmp;
-							j--;
-					}
-				}
-		*/
+			} */
 		System.out.println("After Insertion sorting:");
 		for(int i=0;i<arr.length;i++)
 		{	System.out.print(arr[i]+" ");
