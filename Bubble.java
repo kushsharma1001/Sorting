2c7d2b41cef508ad7828350d373c8906237d37f7
Kush Sharma <kushsharma1001@gmail.com>
2021-05-21 15:23:12 +0530

    Update Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -15,19 +15,24 @@ class Bubble
 			System.out.println();
 		
 		for(int i=0;i<arr.length-1;i++)
-		{	for(int j=0;j<arr.length-1-i;j++)
+		{	int flag=0;
+			for(int j=0;j<arr.length-1-i;j++)
 			{
 				if(arr[j]>arr[j+1])
 				{int temp=arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=temp;
-				
+				flag=1;
 					for(int z=0;z<arr.length;z++)
 								{	System.out.print(arr[z]+" ");
 								}
 							System.out.println();
 				
 				}
+			}
+		 		
+		 	if(flag==0){
+				break;
 			}
 		}
 		System.out.println("After bubble sorting ");
@@ -40,3 +45,19 @@ class Bubble
 }
 
 Best: O(n) Else: O(n^2)
+Demo:
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1. 
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4 
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2 
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+Second Pass: 
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ) 
+( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 ) 
+Now, the array is already sorted, but our algorithm does not know if it is completed. The algorithm needs one whole pass without any swap to know it is sorted.
+Third Pass: 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
