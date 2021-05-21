295cdc7b2d2246b52536dd7affe9623fedc20d6a
Kush Sharma <kushsharma1001@gmail.com>
2021-05-21 21:57:41 +0530

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -89,3 +89,11 @@ class Selection
 		
 	}
 }
+
+Best   Avge     Worst
+O(n^2) O(n^2) O(n^2)
+	
+4 2 5 1 3 : Input array
+1 2 5 4 3 : find minimum from i=0 and then swap min with i=0. Min is 1 so swap with i=0
+1 2 5 4 3 : find min from i=1 and swap min with i=1. Here min is 2 and its already at i=1. So No Swap.
+1 2 3 4 5 : find min from i=2 and it is 3. so swap 3 with i=2.	
